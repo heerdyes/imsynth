184f5cec6524f4c83b68084bf8a6849ce392b76f
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2022-01-09 15:56:56 +0530

    added relative commands

--- machinery.py
+++ machinery.py
@@ -4,10 +4,13 @@ from math import radians,sin,cos,pi
 # global symbol table
 symtab={
     'labels': {},
-    'vars': {},
+    'vars': dict(zip([chr(ord('a')+i) for i in range(26)], [None]*26)),
     'PC': 0,
-    'EQF': False
+    'EQF': False,
+    'SP': []
 }
+# global stack
+stack=[]
 
 # functions
 def readtillspace(ln):
@@ -65,6 +68,9 @@ def ln(x,y,r,th):
     dw.line((x, y, nx, ny),fill=fg,width=1)
     return nx, ny
     
+def lnrel(ax,ay,x,y,r,th):
+    return ln(ax+x,ay+y,r,th)
+    
 def dln(x,y,r,th,n):
     np=x,y
     oldfg=symtab['fgcolor']
@@ -76,15 +82,32 @@ def dln(x,y,r,th,n):
         np=ln(np[0], np[1], r/n, th)
     symtab['fgcolor']=oldfg
     
+def dlnrel(ax,ay,x,y,r,th,n):
+    dln(ax+x,ay+y,r,th,n)
+    
 def drawrect(x,y,w,h):
     if 'canvas' not in symtab:
         raise Exception('canvas not yet created')
     symtab['drawer'].rectangle((x,y,x+w,y+h),fill=symtab['bgcolor'],outline=symtab['fgcolor'])
     
+def drawcircle(x,y,r):
+    if 'canvas' not in symtab:
+        raise Exception('canvas not yet created')
+    symtab['drawer'].ellipse((x,y,x+r,y+r),fill=symtab['bgcolor'],outline=symtab['fgcolor'])
+    
+def drawcirclerel(ax,ay,x,y,r):
+    drawcircle(ax+x,ay+y,r)
+    
+def drawrectrel(ax,ay,x,y,w,h):
+    drawrect(ax+x,ay+y,w,h)
+    
 def write(x,y,msg):
     if 'canvas' not in symtab:
         raise Exception('canvas not yet created')
     symtab['drawer'].text((x,y),msg,fill=symtab['fgcolor'],font=symtab['font'])
+    
+def writerel(ax,ay,x,y,msg):
+    write(ax+x,ay+y,msg)
     
 def drawtxtbox(x,y,w,h,txt):
     if 'canvas' not in symtab:
@@ -103,12 +126,32 @@ def drawarrow(x1,y1,r,th):
     x2=x1+r*cos(ath)
     y2=y1-r*sin(ath)
     dw.line((x1,y1,x2,y2),fill=fg,width=1)
+    drawptr(x2,y2,r,th)
+    
+def drawarrowrel(ax,ay,x1,y1,r,th):
+    drawarrow(ax+x1,ay+y1,r,th)
+    
+def drawdarrow(x1,y1,r,th,n):
+    dln(x1,y1,r,th,n)
+    ath=radians(th)
+    x2=x1+r*cos(ath)
+    y2=y1-r*sin(ath)
+    drawptr(x2,y2,r,th)
+    
+def drawdarrowrel(ax,ay,x1,y1,r,th,n):
+    drawdarrow(ax+x1,ay+y1,r,th,n)
+    
+def drawptr(x1,y1,r,th):
+    dw=symtab['drawer']
+    bg=symtab['bgcolor']
+    fg=symtab['fgcolor']
+    ath=radians(th)
     revth=pi+ath
     rtip=15.0
     ddeg=18.0
     delta=radians(ddeg)
-    dw.line((x2, y2, x2+rtip*cos(revth-delta), y2-rtip*sin(revth-delta)),fill=fg,width=1)
-    dw.line((x2, y2, x2+rtip*cos(revth+delta), y2-rtip*sin(revth+delta)),fill=fg,width=1)
+    dw.line((x1, y1, x1+rtip*cos(revth-delta), y1-rtip*sin(revth-delta)),fill=fg,width=1)
+    dw.line((x1, y1, x1+rtip*cos(revth+delta), y1-rtip*sin(revth+delta)),fill=fg,width=1)
     
 def drawnote(x,y,w,h):
     dw=symtab['drawer']
@@ -117,6 +160,9 @@ def drawnote(x,y,w,h):
     dw.rectangle((x, y, x+w, y+h),fill=None,outline=fg)
     d=8.0
     dw.line((x+w-d, y, x+w, y+d),fill=fg,width=1)
+    
+def drawnoterel(ax,ay,x,y,w,h):
+    drawnote(ax+x,ay+y,w,h)
     
 def setfnt(fntf):
     global symtab
@@ -144,26 +190,63 @@ def execinst(cmd,args):
     elif cmd=='ln':
         params=[int(tokinterp(m)) for m in args.split(' ')]
         ln(*params)
+    elif cmd=='lnr':
+        params=[int(tokinterp(m)) for m in args.split(' ')]
+        lnrel(*params)
     elif cmd=='dln':
         params=[int(tokinterp(m)) for m in args.split(' ')]
         dln(*params)
+    elif cmd=='dlnr':
+        params=[int(tokinterp(m)) for m in args.split(' ')]
+        dlnrel(*params)
     elif cmd=='box':
         params=[int(tokinterp(m)) for m in args.split(' ')]
         drawrect(*params)
+    elif cmd=='boxr':
+        params=[int(tokinterp(m)) for m in args.split(' ')]
+        drawrectrel(*params)
+    elif cmd=='gol':
+        params=[int(tokinterp(m)) for m in args.split(' ')]
+        drawcircle(*params)
+    elif cmd=='golr':
+        params=[int(tokinterp(m)) for m in args.split(' ')]
+        drawcirclerel(*params)
     elif cmd=='txt':
         car,cdr=readtillspace(args)
         x=int(tokinterp(car))
         car,cdr=readtillspace(cdr)
         y=int(tokinterp(car))
         write(x,y,cdr)
+    elif cmd=='txtr':
+        car,cdr=readtillspace(args)
+        ax=int(tokinterp(car))
+        car,cdr=readtillspace(cdr)
+        ay=int(tokinterp(car))
+        car,cdr=readtillspace(cdr)
+        x=int(tokinterp(car))
+        car,cdr=readtillspace(cdr)
+        y=int(tokinterp(car))
+        writerel(ax,ay,x,y,cdr)
     elif cmd=='fnt':
         setfnt(args)
     elif cmd=='arrow':
         params=[int(tokinterp(m)) for m in args.split(' ')]
         drawarrow(*params)
+    elif cmd=='arrowr':
+        params=[int(tokinterp(m)) for m in args.split(' ')]
+        drawarrowrel(*params)
+    elif cmd=='darrow':
+        params=[int(tokinterp(m)) for m in args.split(' ')]
+        drawdarrow(*params)
+    elif cmd=='darrowr':
+        params=[int(tokinterp(m)) for m in args.split(' ')]
+        drawdarrowrel(*params)
     elif cmd=='note':
         params=[int(tokinterp(m)) for m in args.split(' ')]
         drawnote(*params)
+    elif cmd=='noter':
+        params=[int(tokinterp(m)) for m in args.split(' ')]
+        drawnoterel(*params)
     elif cmd=='txtbox':
         car,cdr=readtillspace(args)
         x=int(tokinterp(car))
@@ -207,4 +290,3 @@ def execinst(cmd,args):
         symtab['EQF']=int(o1)==int(o2)
     elif cmd=='lbl':
         return
-
